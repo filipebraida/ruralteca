d405b57faa3b1fd35a19a2adb3853b2b046f4c55
Filipe Braida <filipebraida@gmail.com>
2022-02-16 15:42:39 -0300

    criando video e curso

---
+++ database/migrations/1645036155271_courses.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Courses extends BaseSchema {
+  protected tableName = 'courses'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('name').notNullable()
+      table.string('description').notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
