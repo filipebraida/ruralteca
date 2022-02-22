afb337a98ea3d9293913b2af67d16e7a9f7fa19b
Filipe Braida <filipebraida@gmail.com>
2022-02-22 10:00:19 -0300

    criando a pagina index

--- start/routes.ts
+++ start/routes.ts
@@ -21,7 +21,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async ({ view }) => {
-  return view.render('welcome')
+  return view.render('index')
 })
 
 Route.get('/courses/:id', 'CoursesController.show')
